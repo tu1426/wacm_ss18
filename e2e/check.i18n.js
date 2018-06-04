9c5e0bf4b673520ad3e23e00a5df50a3991196f8
e8606876 <josef.glas@gmx.net>
2018-06-04 10:58:04 +0200

    e2e tests modified, refinement

--- e2e/check.i18n.js
+++ e2e/check.i18n.js
@@ -2,7 +2,7 @@ module.exports = {
     'Landing Page': function (browser) {
         browser
             .url('https://localhost:8443/login')
-            .pause(3000)
+            .pause(5000)
             .waitForElementVisible('body', 5000)
             .assert.urlContains('login')
             .assert.elementPresent('BUTTON')
@@ -14,7 +14,7 @@ module.exports = {
             .setValue('input#usernameInput[type=text]', 'user')
             .setValue('input#passwordInput[type=password]', 'password')
             .click('button.btn.btn-primary')
-            .pause(3000)
+            .pause(5000)
             .assert.urlContains('home')
             .assert.containsText("body", "Welcome, you are logged in!")
     },
@@ -22,7 +22,7 @@ module.exports = {
     'Check german translation': function (browser) {
         browser
             .click('ul.nav.navbar-nav')
-            .pause(3000)
+            .pause(5000)
             .assert.containsText('body', 'Gesundheitsdaten')
             .end();
     }
--- e2e/valid.login.js
+++ e2e/valid.login.js
@@ -2,7 +2,7 @@ module.exports = {
     'Landing Page': function (browser) {
         browser
             .url('https://localhost:8443/login')
-            .pause(3000)
+            .pause(5000)
             .waitForElementVisible('body', 5000)
             .assert.urlContains('login')
             .assert.elementPresent('BUTTON')
@@ -14,7 +14,7 @@ module.exports = {
             .setValue('input#usernameInput[type=text]', 'user')
             .setValue('input#passwordInput[type=password]', 'password')
             .click('button.btn.btn-primary')
-            .pause(3000)
+            .pause(5000)
             .assert.urlContains('home')
             .assert.containsText("body", "Welcome, you are logged in!")
     },
@@ -22,23 +22,23 @@ module.exports = {
     'A User who is logged in cannot access the Login page again': function (browser) {
         browser
             .url('https://localhost:8443/login')
-            .pause(3000)
+            .pause(5000)
             .assert.urlContains('home')
     },
 
     'A user can refresh the page and redirect to the same page': function (browser) {
         browser
             .refresh()
-            .pause(3000)
+            .pause(5000)
             .assert.urlContains('home')
     },
 
     'A User can logout': function (browser) {
         browser
             .click('ul.nav.navbar-nav.navbar-right')
-            .pause(3000)
+            .pause(5000)
             .click('ul.dropdown-menu')
-            .pause(3000)
+            .pause(5000)
             .assert.urlContains('login')
             .assert.containsText('body', 'Please log in!')
             .end();
