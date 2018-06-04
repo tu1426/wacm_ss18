c217f12a0760381022c42c74df2a25bac4d3453a
e8606876 <josef.glas@gmx.net>
2018-06-04 10:23:50 +0200

    e2e tests modified, try --headless

--- e2e/valid.login.js
+++ e2e/valid.login.js
@@ -15,7 +15,7 @@ module.exports = {
             .setValue('input#usernameInput[type=text]', 'user')
             .setValue('input#passwordInput[type=password]', 'password')
             .click('button.btn.btn-primary')
-            .pause(5000)
+            .pause(3000)
             .assert.urlContains('home')
             .assert.containsText("body", "Welcome, you are logged in!")
     },
@@ -23,23 +23,23 @@ module.exports = {
     'A User who is logged in cannot access the Login page again': function (browser) {
         browser
             .url('https://localhost:8443/login')
-            .pause(5000)
+            .pause(3000)
             .assert.urlContains('home')
     },
 
     'A user can refresh the page and redirect to the same page': function (browser) {
         browser
             .refresh()
-            .pause(5000)
+            .pause(3000)
             .assert.urlContains('home')
     },
 
     'A User can logout': function (browser) {
         browser
             .click('ul.nav.navbar-nav.navbar-right')
-            .pause(5000)
+            .pause(3000)
             .click('ul.dropdown-menu')
-            .pause(5000)
+            .pause(3000)
             .assert.urlContains('login')
             .assert.containsText('body', 'Please log in!')
             .end();
