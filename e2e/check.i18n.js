6fc7bef5eb14357d261d2aa0b55a6ff6b6ee4e04
e8606876 <josef.glas@gmx.net>
2018-06-04 12:17:40 +0200

    e2e tests modified, now only firefox

--- e2e/check.i18n.js
+++ e2e/check.i18n.js
@@ -1,30 +1,40 @@
 module.exports = {
-    'Landing Page': function (client) {
-        client
+    'Landing Page': function (browser) {
+        browser
             .url('https://localhost:8443/login')
-            .pause(9000)
+            .pause(3000)
             .waitForElementVisible('body', 5000)
             .assert.urlContains('login')
+            .assert.title('WACM_Group13')
+            .assert.elementPresent('BUTTON')
             .assert.containsText('body', 'Please log in!')
     },
 
-    'A User can login': function (client) {
-        client
+    'A User can login': function (browser) {
+        browser
             .setValue('input#usernameInput[type=text]', 'user')
             .setValue('input#passwordInput[type=password]', 'password')
             .click('button.btn.btn-primary')
-            .pause(9000)
+            .pause(3000)
             .assert.urlContains('home')
             .assert.containsText("body", "Welcome, you are logged in!")
     },
 
-    'Check german translation': function (client) {
-        client
+    'Check english translation': function (browser) {
+        browser
             .click('ul.nav.navbar-nav')
-            .pause(9000)
-            .assert.containsText('body', 'Gesundheitsdaten')
+            .pause(5000)
+            .assert.containsText('body', 'Health data')
             .end();
     }
+
+
+
+
+
+
+
+
 
 
 
--- e2e/valid.login.js
+++ e2e/valid.login.js
@@ -1,43 +1,45 @@
 module.exports = {
-    'Landing Page': function (client) {
-        client
+    'Landing Page': function (browser) {
+        browser
             .url('https://localhost:8443/login')
-            .pause(5000)
+            .pause(3000)
             .waitForElementVisible('body', 5000)
             .assert.urlContains('login')
+            .assert.title('WACM_Group13')
+            .assert.elementPresent('BUTTON')
             .assert.containsText('body', 'Please log in!')
     },
 
-    'A User can login': function (client) {
-        client
+    'A User can login': function (browser) {
+        browser
             .setValue('input#usernameInput[type=text]', 'user')
             .setValue('input#passwordInput[type=password]', 'password')
             .click('button.btn.btn-primary')
-            .pause(5000)
+            .pause(3000)
             .assert.urlContains('home')
             .assert.containsText("body", "Welcome, you are logged in!")
     },
 
-    'A User who is logged in cannot access the Login page again': function (client) {
-        client
+    'A User who is logged in cannot access the Login page again': function (browser) {
+        browser
             .url('https://localhost:8443/login')
-            .pause(5000)
+            .pause(3000)
             .assert.urlContains('home')
     },
 
-    'A user can refresh the page and redirect to the same page': function (client) {
-        client
+    'A user can refresh the page and redirect to the same page': function (browser) {
+        browser
             .refresh()
-            .pause(5000)
+            .pause(3000)
             .assert.urlContains('home')
     },
 
-    'A User can logout': function (client) {
-        client
+    'A User can logout': function (browser) {
+        browser
             .click('ul.nav.navbar-nav.navbar-right')
-            .pause(5000)
+            .pause(3000)
             .click('ul.dropdown-menu')
-            .pause(5000)
+            .pause(3000)
             .assert.urlContains('login')
             .assert.containsText('body', 'Please log in!')
             .end();
