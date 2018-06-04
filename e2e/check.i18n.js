d953a440987f10f9fcc40d9dee23dff6c0e60b1e
e8606876 <josef.glas@gmx.net>
2018-06-04 13:06:08 +0200

    e2e tests modified, now only firefox

--- e2e/check.i18n.js
+++ e2e/check.i18n.js
@@ -1,8 +1,8 @@
 module.exports = {
-    'Landing Page': function (browser) {
-        browser
+    'Landing Page': function (client) {
+        client
             .url('https://localhost:8443/login')
-            .pause(3000)
+            .pause(5000)
             .waitForElementVisible('body', 5000)
             .assert.urlContains('login')
             .assert.title('WACM_Group13')
@@ -10,18 +10,18 @@ module.exports = {
             .assert.containsText('body', 'Please log in!')
     },
 
-    'A User can login': function (browser) {
-        browser
+    'A User can login': function (client) {
+        client
             .setValue('input#usernameInput[type=text]', 'user')
             .setValue('input#passwordInput[type=password]', 'password')
             .click('button.btn.btn-primary')
-            .pause(3000)
+            .pause(5000)
             .assert.urlContains('home')
             .assert.containsText("body", "Welcome, you are logged in!")
     },
 
-    'Check english translation': function (browser) {
-        browser
+    'Check english translation': function (client) {
+        client
             .click('ul.nav.navbar-nav')
             .pause(5000)
             .assert.containsText('body', 'Health data')
