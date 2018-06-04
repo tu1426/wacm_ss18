58f268f64966dfc13f06e4f279264554b989d0d0
e8606876 <josef.glas@gmx.net>
2018-06-04 16:32:13 +0200

    e2e tests modified, testing

--- e2e/basic.spec.js
+++ e2e/basic.spec.js
@@ -1,17 +1,19 @@
 module.exports = {
-  'check if index available'(client) {
+
+    'check if index available'(client) {
     client
       .url('https://localhost:8443')
       .waitForElementVisible('body', 3000)
-      .expect.element('h1').to.be.present
-      .assert.title('WACM_Group13')
+      .expect.element('h1').to.be.present;
+    client.verify.title('WACM_Group13');
     client.end();
   },
+
   'check if register available'(client) {
     client
       .url('https://localhost:8443/register')
       .waitForElementVisible('body', 3000)
       .expect.element('div').to.be.present;
     client.end();
-  },
+  }
 };
