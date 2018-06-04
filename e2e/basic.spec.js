b34bf9aefe00b602affd127dee930b694725fb54
e8606876 <josef.glas@gmx.net>
2018-06-04 17:29:40 +0200

    e2e tests modified, now run in web-server-container

--- e2e/basic.spec.js
+++ e2e/basic.spec.js
@@ -1,5 +1,4 @@
 module.exports = {
-
     'check if index available'(client) {
     client
       .url('https://localhost:8443')
@@ -7,15 +6,6 @@ module.exports = {
       .expect.element('h1').to.be.present;
     client.end();
   },
-
-    'check title'(client) {
-        client
-            .url('https://localhost:8443')
-            .waitForElementVisible('body', 3000)
-            .verify.title('WACM_Group13');
-        client.end();
-    },
-
   'check if register available'(client) {
     client
       .url('https://localhost:8443/register')
